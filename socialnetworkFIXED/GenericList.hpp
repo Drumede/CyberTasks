a77f0f498807ee4f3dcb22c3043ef5d547561f85
Drumede <eitan.ula@gmail.com>
2026-01-06 19:02:15 +0200

    yeah

---
+++ socialnetworkFIXED/GenericList.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <string>
+
+/////////////////////////////////////////////
+//       GenericNode Definition            //
+//                                         //
+// Please note: the definitions and         //
+// implementations are on the same file    //
+// because we use templates                //
+//                                         //
+/////////////////////////////////////////////
+
+template<class T>
+class GenericNode
+{
+public:
+	GenericNode(const T item);
+
+	// getters
+	GenericNode<T>* get_next() const;
+	T& get_item();
+
+	// setters
+	void set_next(GenericNode<T>* next);
+	void set_item(const T new_item);
+
+private:
+	T _item;
+	GenericNode<T>* _next;
+};
+
+/////////////////////////////////////////////
+//       GenericList Definition            //
+//                                         //
+// Please note: the definitions and         //
+// implementations are on the same file    //
+// because we use templates                //
+//                                         //
+/////////////////////////////////////////////
+
+template<class T>
+class GenericList
+{
+public:
+	GenericList();
+	~GenericList();
+
+	// getters
+	GenericNode<T>* get_first() const;
+
+	// setters
+	void set_first(GenericNode<T>* first);
+
+	// adds an object to the end of the list
+	void add(const T device_to_add);
+
+private:
+	GenericNode<T>* _first;
+};
+
+/////////////////////////////////////////////
+//       GenericNode implementation        //
+//                                         //
+/////////////////////////////////////////////
+
+template<class T>
+GenericNode<T>::GenericNode(const T item)
+{
+	this->_item = item;
+	this->_next = nullptr;
+}
+
+template<class T>
+GenericNode<T>* GenericNode<T>::get_next() const
+{
+	return this->_next;
+}
+
+template<class T>
+T& GenericNode<T>::get_item()
+{
+	return this->_item;
+}
+
+template<class T>
+void GenericNode<T>::set_next(GenericNode<T>* next)
+{
+	this->_next = next;
+}
+
+template<class T>
+void GenericNode<T>::set_item(const T new_item)
+{
+	this->_item = new_item;
+}
+
+/////////////////////////////////////////////
+//       GenericList implementation        //
+//                                         //
+/////////////////////////////////////////////
+
+template<class T>
+GenericList<T>::GenericList()
+{
+	this->_first = nullptr;
+}
+
+template<class T>
+GenericList<T>::~GenericList()
+{
+	GenericNode<T>* temp;
+	while (this->_first != nullptr)
+	{
+		temp = this->_first;
+		this->_first = this->_first->get_next();
+		delete temp;
+	}
+}
+
+template<class T>
+GenericNode<T>* GenericList<T>::get_first() const
+{
+	return this->_first;
+}
+
+template<class T>
+void GenericList<T>::set_first(GenericNode<T>* first)
+{
+	this->_first = first;
+}
+
+template<class T>
+void GenericList<T>::add(const T device_to_add)
+{
+	GenericNode<T>* new_node = new GenericNode<T>(device_to_add);
+
+	if (this->_first == nullptr)
+	{
+		this->_first = new_node;
+	}
+	else
+	{
+		GenericNode<T>* curr = this->_first;
+		while (curr->get_next() != nullptr)
+		{
+			curr = curr->get_next();
+		}
+		curr->set_next(new_node);
+	}
+}
